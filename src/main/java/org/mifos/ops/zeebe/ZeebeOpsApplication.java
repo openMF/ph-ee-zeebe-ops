423a79090001e1efb3b04fb01040aa8523db4e90
Danish Jamal <danishjamal.104@gmail.com>
2022-05-19 17:22:19 +0530

    Custom rest client bean definition added

--- src/main/java/org/mifos/ops/zeebe/ZeebeOpsApplication.java
+++ src/main/java/org/mifos/ops/zeebe/ZeebeOpsApplication.java
@@ -5,16 +5,25 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ZeebeOpsApplication {
+
+    @Value("${spring.data.elasticsearch.client.reactive.endpoints}")
+    private String contactPoint;
 
     @Bean
     public ObjectMapper objectMapper() {
@@ -25,6 +34,21 @@ public class ZeebeOpsApplication {
                 .setSerializationInclusion(JsonInclude.Include.NON_NULL)
                 .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    @Bean
+    public RestHighLevelClient client() {
+        ClientConfiguration clientConfiguration
+                = ClientConfiguration.builder()
+                .connectedTo(contactPoint)
+                .build();
+
+        return RestClients.create(clientConfiguration).rest();
+    }
+
+    @Bean
+    public ElasticsearchOperations elasticsearchTemplate() {
+        return new ElasticsearchRestTemplate(client());
     }
 
     @Bean
